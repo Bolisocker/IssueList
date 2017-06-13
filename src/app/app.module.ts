95c742a7e1d326cbd42e86a18b43eba0776a5de3
Imanol Jimenez <ijimenez@irontec.com>
2017-06-13 23:14:07 +0200

    dependency fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AppComponent} from './app.component';
-import {Ng2PaginationModule} from 'ng2-pagination';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { Ng2PaginationModule } from 'ng2-pagination';
 
 @NgModule({
   declarations: [
@@ -14,8 +14,10 @@ import {Ng2PaginationModule} from 'ng2-pagination';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot(),
+    NgbModule,
     Ng2PaginationModule
+  ],
+  entryComponents: [
   ],
   providers: [],
   bootstrap: [AppComponent]
